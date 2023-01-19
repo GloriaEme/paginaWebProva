9b4a1ba2a0dbd3f3500649d7c20f3f08ae3562b0
Gloria Molina <gloriamolina44@gmail.com>
2023-01-19 16:06:10 +0100

    estructura

---
+++ codi/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-row>
+        <v-col>
+          <nuxt-link to="/">Inici</nuxt-link>
+          <nuxt-link to="/empresa">Empresa</nuxt-link>
+          <nuxt-link to="/productes">Productes</nuxt-link>
+          <nuxt-link to="/privacitat">Privacitat</nuxt-link>
+          <nuxt-link to="/contacte">Contacte</nuxt-link>
+        </v-col>
+      </v-row>
+      <Nuxt/>
+    </v-main>
+  </v-app>
+</template>
+
+S
---
+++ codi/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Inici</h1>
+        <!-- <nuxt-link to="/empresa">Empresa</nuxt-link> -->
+      </v-col>
+    </v-row>
+  </v-container>
+  </template>
